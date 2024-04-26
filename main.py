da9d8da1f923c67ec95be16bbd83d08fd0565966
YOVA-DUALB-MINT <glombardi@fi.uba.ar>
2024-04-26 14:20:22 -0300

    some testing

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
-print("Hello World")
+import os
+
 or_path = "/home/giovanni/Desktop/github-repos/file-mover/origin"
 des_path = "/home/giovanni/Desktop/github-repos/file-mover/dest"
 
+class FileMover():
+    def __init__(self, opath=None, dpath=None) -> None:
+        self.origin = opath
+        self.dest = dpath
+    
+    def set_or(self, path):
+        self.origin = path
+
+    def set_dest(self, path):
+        self.dest = path
+
+    def get_or(self):
+        return self.origin
+
+    def get_dest(self):
+        return self.dest
+    
+my_fyle_mover = FileMover(or_path, des_path) # Can initiate only with 1 param why?
+#my_fyle_mover.set_dest = des_path
+
+dest_list = os.listdir(my_fyle_mover.get_dest())
+print("Files and directories in Destination:")
+print(dest_list)
+
+or_list = os.listdir(my_fyle_mover.get_or())
+print("Files and directories in Origin:")
+print(or_list)
