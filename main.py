b79402e0457c0e8b8436ebe5128cabe947d79c78
YOVA-DUALB-MINT <glombardi@fi.uba.ar>
2024-04-29 18:18:02 -0300

    few changes

--- main.py
+++ main.py
@@ -4,29 +4,35 @@ or_path = "/home/giovanni/Desktop/github-repos/file-mover/origin"
 des_path = "/home/giovanni/Desktop/github-repos/file-mover/dest"
 
 class FileMover():
-    def __init__(self, opath=None, dpath=None) -> None:
-        self.origin = opath
-        self.dest = dpath
+    def __init__(self):
+        self.origin = None
+        self.dest = None
+        self.wantsFolder = False
     
-    def set_or(self, path):
+    def setOr(self, path):
         self.origin = path
 
-    def set_dest(self, path):
+    def setDest(self, path):
         self.dest = path
 
-    def get_or(self):
+    def getOr(self):
         return self.origin
 
-    def get_dest(self):
+    def getDest(self):
         return self.dest
     
-my_fyle_mover = FileMover(or_path, des_path) # Can initiate only with 1 param why?
-#my_fyle_mover.set_dest = des_path
+    def wantFolder(self):
+        self.wantsFolder = True
+    
+    def moveToDest(self):
+        # Pending...
+        return "OK"
+    
+    
+mfm = FileMover()
+mfm.setOr(or_path)
+mfm.setDest(des_path)
 
-dest_list = os.listdir(my_fyle_mover.get_dest())
-print("Files and directories in Destination:")
-print(dest_list)
-
-or_list = os.listdir(my_fyle_mover.get_or())
-print("Files and directories in Origin:")
-print(or_list)
+# TEST
+print(f"Files in origin: { os.listdir(mfm.getOr()) }")
+print(f"Files in dest: { os.listdir(mfm.getDest()) }")
