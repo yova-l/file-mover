cf820f113663f669d266b6466a99f8a999b5eb9d
YOVA-DUALB-MINT <glombardi@fi.uba.ar>
2024-05-28 13:14:13 -0300

    logic ready

--- main.py
+++ main.py
@@ -3,22 +3,35 @@ from model.FileMover import FileMover
 jpgs_path = "./jpgs/"
 raws_path = "./raws/"
 raw_folder_name = "MyRaws"
+jpg_ext = ".jpg"
+raw_ext = ".dng"
+dump_path = "./"
 
 
-## CASO QUIERE MOVER, ESTAN TODOS LOS FILES, Y QUIERE CARPETA // OK
+## CASO QUIERE COPIAR, ESTAN TODOS LOS FILES, Y QUIERE CARPETA // OK
 # mfm = FileMover()
+
 # mfm.setJpgsPath(jpgs_path)
 # mfm.setRawsPath(raws_path)
+
 # mfm.wantFolder()
 # mfm.setFolderName(raw_folder_name)
+
 # mfm.moveRaws()
 #############################################################################
 
 ## CASO QUIERE MOVER, ESTAN TODOS LOS FILES, Y NO QUIERE CARPETA // OK
 # mfm = FileMover()
+
 # mfm.setJpgsPath(jpgs_path)
 # mfm.setRawsPath(raws_path)
+
+# mfm.setDumpDest(jpgs_path)
+# mfm.dontWantDump()
+# mfm.dontWantFolder()
+
 # mfm.setFolderName(raw_folder_name) # Esto es ignorado OK
+
 # mfm.moveRaws()
 ###############################################################################
 
@@ -28,7 +41,6 @@ raw_folder_name = "MyRaws"
 # mfm.setRawsPath(raws_path)
 
 # mfm.wantFolder()
-# mfm.dontWantFolder()
 
 # mfm.setFolderName(raw_folder_name)
 # mfm.moveRaws()
@@ -37,16 +49,32 @@ raw_folder_name = "MyRaws"
 
 ## CASO QUIERE COPIAR, ESTAN TODOS (aunque no esten funciona ahora OK!!!)
 # mfm = FileMover()
+
 # mfm.setJpgsPath(jpgs_path)
 # mfm.setRawsPath(raws_path)
+# mfm.setDumpDest(jpgs_path)
+# mfm.setFolderName(raw_folder_name)
+# mfm.setJpgExt(jpg_ext)
+# mfm.setRawExt(raw_ext)
 
+# mfm.wantDump()
 # mfm.wantFolder()
-# mfm.wantCopy()
 # mfm.dontWantCopy()
 
-# mfm.setFolderName(raw_folder_name)
 # mfm.moveRaws()
 
 ###############################################################################
 # SE AGREGO PARA DEFINIR LAS EXTENSIONES Y SETEAR SI QUIERO O NO DUMP, CHECKEAR
 # REacomodar la clase
+# mfm = FileMover()
+# mfm.setJpgsPath(jpgs_path)
+# mfm.setRawsPath(raws_path)
+
+# mfm.wantFolder()
+# mfm.wantCopy()
+
+# mfm.setJpgExt(jpg_ext)
+# mfm.setRawExt(raw_ext)
+
+# mfm.setFolderName(raw_folder_name)
+# mfm.moveRaws()
--- model/FileMover.py
+++ model/FileMover.py
@@ -1,109 +1,102 @@
 import os
 import shutil
 import filecmp
+import platform
+
+OS = platform.system()
 
 class FileMover():
     def __init__(self):
-        self._jpgsPath = None
-        self._rawsPath = None
-        self._wantsFolder = False
-        self._finalFolderName = ""
-        self._rawEnding = ".dng"
-        self._jpgEnding = ".jpg"
+        self._jpgsPath = "./"
+        self._rawsPath = "./"
+        self._dumpPath = "./"
+        self._wantsFolder = True
+        self._wantsDump = True
+        self._finalFolderName = "MyRaws"
+        self._rawExtension = ".dng"
+        self._jpgExtension = ".jpg"
         self._notFounded = []
-        self._isCopy = False
-        self._dumpPath = ""
-    
-    def _dumpNotFounded(self):
-        if len(self._notFounded) == 0: return
+        self._isCopy = True
 
-        with open("dump.txt", "w") as f:
+    # Setters
+    def setJpgExt(self, extension: str) -> None: self._jpgExtension = extension
+    def setRawExt(self, extension: str) -> None: self._rawExtension = extension
+    def setFolderName(self, name: str) -> None: self._finalFolderName = name
+    def setJpgsPath(self, path: str) -> None: self._jpgsPath = path
+    def setRawsPath(self, path: str) -> None: self._rawsPath = path
+    def wantFolder(self) -> None: self._wantsFolder = True
+    def dontWantFolder(self) -> None: self._wantsFolder = False
+    def wantCopy(self) -> None: self._isCopy = True
+    def dontWantCopy(self) -> None: self._isCopy = False
+    def wantDump(self) -> None: self._wantsDump = True
+    def dontWantDump(self) -> None: self._wantsDump = False
+    def setDumpDest(self, path: str): self._dumpPath = path
+
+    def _dumpNotFounded(self):
+        if len(self._notFounded) == 0 or not self._wantsDump: return
+
+        with open(f"{self._dumpPath}dump.txt", "w") as f:
             for path in self._notFounded:
                 # Writing data to a file
                 f.write(f"Not founded: {os.path.abspath(path)}\n")
  
         self._notFounded = []
 
-
-    def _getJpgsNames(self, path):
+    def _getJpgsNames(self, path: str) -> set:
+        """
+        It will search for the files that meet the set extension in the already set folder.
+        Returns a set containing all the corresponding paths
+        """
         jpgFileNames = set(
-            filter(lambda x: ".jpg" in x, os.listdir(path))
+            filter(lambda x: f"{self._jpgExtension}" in x, os.listdir(path))
         )
         return jpgFileNames
 
-    def _extractJpgName(self, fullPath):
+    def _extractJpgName(self, fullPath: str):
+        # fullPath is going to be somthing like: /home/Desktop/pics/jpg/selection/yes/123456.jpeg
         name = ""
-        fullPath = fullPath[:-4] # .jpg out
-        for i in range(len(fullPath)-1, -1, -1):
+        point_index = len(fullPath) - 1
+
+        # Dismiss the file extension
+        for i in range(point_index, -1, -1):
+            if fullPath[i] == ".": break
+            point_index -= 1
+
+        # Extract the filename
+        for i in range(point_index-1, -1, -1):
             c = fullPath[i]
-            if c == "/": break
+            if c == "/" or c == "\\": break
             name += c
     
         return name[::-1]
         
-    def _getRawPaths(self, path, jpgsSet):
-        # Now gotta grab from / (...) .jpg
-        # And compose path + (...) + .dng/raw/etc
-
+    def _getRawPaths(self, path: str, jpgsSet: set) -> list:
+        """Given a set of jpgs paths, it will search for all the raw corresponding pairs and return them 
+        in a list.
+        """
         rawsPaths = []
 
         for jpgPath in jpgsSet:
             jpgName = self._extractJpgName(jpgPath)
-            rawPath = path + jpgName + self._rawEnding
+            rawPath = path + jpgName + self._rawExtension
             rawsPaths.append(rawPath)
 
         return rawsPaths
 
-    def _getDestRawPath(self, oriRawPath, finalDirectoryPath):
+    def _getDestinationRawPath(self, oriRawPath, finalDirectoryPath):
         fileName = ""
 
         for i in range(len(oriRawPath) - 1, -1, -1):
             c = oriRawPath[i]
-            if c == "/": break
+            if c == "/" or c == "\\": break
             fileName += c
-    
+
+        # WHAT IF WE ARE ON WIDNOWS?? Check the above OS variable might be a solution, test it after
         return finalDirectoryPath + "/" + fileName[::-1]
             
-    def setFolderName(self, name):
-        self._finalFolderName = name
-    
-    def setJpgsPath(self, path):
-        self._jpgsPath = path
-
-    def setRawsPath(self, path):
-        self._rawsPath = path
-
-    def getJpgsPath(self):
-        return self._jpgsPath
-
-    def getRawsPath(self):
-        return self._rawsPath
-    
-    def wantFolder(self):
-        self._wantsFolder = True
-    
-    def dontWantFolder(self):
-        self._wantsFolder = False
-
-    def wantCopy(self):
-        self._isCopy = True
-    
-    def dontWantCopy(self):
-        self._isCopy = False
-
-    def wantDump():pass
-
-    def dontWantDump():pass
-        
-    def setDumpDest():pass
-
-    def setJpgExtension(extension):pass
-
-    def setRawExtension(extension):pass
-
     def moveRaws(self):
         jpgsSet = self._getJpgsNames(self._jpgsPath)
-        oriRawsPathList = self._getRawPaths(self._rawsPath, jpgsSet)
+        originRawsPathList = self._getRawPaths(self._rawsPath, jpgsSet)
         finalPath = self._jpgsPath
 
         if self._wantsFolder:
@@ -111,26 +104,28 @@ class FileMover():
             if not os.path.exists(finalPath):
                 os.mkdir(finalPath)
 
-        for oriRawImgPath in oriRawsPathList:
-            if not os.path.exists(oriRawImgPath): # Is the raw in the expected folder?
-                self._notFounded.append(oriRawImgPath)
+        for originRawImgPath in originRawsPathList:
+            if not os.path.exists(originRawImgPath): # Is the raw in the expected folder?
+                self._notFounded.append(originRawImgPath)
                 continue
 
-            shutil.copy2(oriRawImgPath, finalPath)
+            shutil.copy2(originRawImgPath, finalPath)
             safe_counter = 0
-            destRawImgpath = self._getDestRawPath(oriRawImgPath, finalPath)
+            destinationRawImgPath = self._getDestinationRawPath(originRawImgPath, finalPath)
+            succesfull_copy = filecmp.cmp(originRawImgPath, destinationRawImgPath)
 
-            while not filecmp.cmp(oriRawImgPath, destRawImgpath):
+            while not succesfull_copy:
                 if os.path.exists(finalPath):
                     os.remove(finalPath)
-                shutil.copy2(oriRawImgPath, finalPath)
+                shutil.copy2(originRawImgPath, finalPath)
                 safe_counter += 1
                 if safe_counter == 20: 
                     raise IOError
+                succesfull_copy = filecmp.cmp(originRawImgPath, destinationRawImgPath)
             
             # It's checked that the files are the same, delete the original (only if it's not a copy)
             if not self._isCopy:
-                os.remove(oriRawImgPath)
+                os.remove(originRawImgPath)
 
         self._dumpNotFounded()
          
